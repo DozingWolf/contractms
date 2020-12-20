4d44c2d21e9039621985bf207e6e44f4b0effd56
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2020-12-20 16:51:21 +0800

    添加方法处理数据库结果集，使成为容易被读取的json

--- contractms_backend/base.py
+++ contractms_backend/base.py
@@ -56,4 +56,40 @@ class AppInitial(object):
     
     def getMainFunc(self):
         return self.dbConnect,self.sysLog
+    
+    def transCustomList2Dict(self,inputlist=[],colnamemodel=[]):
+        # 用于处理数据库反馈list转换成可被前端读取的dict，供后续转json使用
+        fbkMessage = {'status':'','statinfo':'','dataload':{}}
+        if inputlist == []:
+            fbkMessage.update({'status':'0','statinfo':'没有传入的参数inputlist'})
+            return fbkMessage
+        if colnamemodel == []:
+            fbkMessage.update({'status':'0','statinfo':'没有传入的参数colnamemodel'})
+            return fbkMessage
+        if len(inputlist[0]) != len(colnamemodel):
+            statinfo = '传入的数据栏目数%d个和给定的dict模板栏目数%d个不匹配，请检查参数'%(len(inputlist[0]),len(colnamemodel))
+            fbkMessage.update({'status':'0','statinfo':statinfo})
+            return fbkMessage
+        else:
+            rowcount = len(inputlist)
+            columncount = len(inputlist[0])
+            self.sysLog.debug('data result set has %d row(s) %d columns,'%(rowcount,columncount))
+            # dataload = {'rowcount':rowcount,'dataload':{}}
+            innerdatalist = []
+            innerrowlist = []
+            datadict = {}
+            for rowno,datarow in enumerate(inputlist):
+                self.sysLog.debug('row no = %d,data row = %s'%(rowno,datarow))
+                # 组合列标题和数据值，并转换成dict供后续处理
+                tempdataload = dict(zip(colnamemodel,datarow))
+                self.sysLog.debug('tempdataload is :%s'%tempdataload)
+                innerrowlist.append(rowno)
+                innerdatalist.append(tempdataload)
+                # for datano,datacol in enumerate(inputlist[rowno]):
+                #     self.sysLog.debug('No.%d ==> %s,colname = %s'%(datano,datacol,colnamemodel[datano]))
+            datadict.update(dict(zip(innerrowlist,innerdatalist)))
+            self.sysLog.debug('inner datalist is %s'%innerdatalist)
+            fbkMessage.update({'dataload':datadict})
+            fbkMessage.update({'status':'1','statinfo':'转换成功!!'})
 
+            return fbkMessage
